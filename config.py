1f7c3def66780569f6ba225f402f2746c04e338a
Raz_at <razbaidya1@gmail.com>
2025-02-04 23:34:18 +0545

    Remove config.py from GitHub

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.urandom(24)
-    GOOGLE_CLIENT_ID = '332802899388-okflj9q5ffrsoktkkvpbs6apuibu9j3n.apps.googleusercontent.com'
-    GOOGLE_CLIENT_SECRET = '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'
-    TO_EMAIL = 'razbaidya1@gmail.com'
