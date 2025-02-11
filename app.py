d9f2bc22ef4db65d9b055c7f6b960f4b0d1c2b64
Raz-at <razbaidya1@gmail.com>
2025-02-11 16:27:46 +0545

    changes in d

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from flask_sqlalchemy import SQLAlchemy
 from models import User
+import uuid
+from sqlalchemy import select
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
@@ -22,6 +24,7 @@ DB_URL = os.getenv("DATABASE_URL")
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DB_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -66,12 +69,19 @@ def home():
 @app.route('/<sender_email>')
 def index(sender_email):    
     # key = app.config['MY_ENCRYPTION_KEY']
-    key = MY_ENCRYPTION_KEY
+    key = MY_ENCRYPTION_KEY  
 
-    decryptrd_email = decrypt_user_email(sender_email,key)
+    decryptrd_email_user_id = decrypt_user_email(sender_email,key)
 
-    session['user_name_message'] = decryptrd_email
+    # session['user_name_message'] = decryptrd_email
+
+    session['UNIQUE_USER_ID'] = decryptrd_email_user_id
+
+    exists = db.session.query(User).filter(User.sender_id == decryptrd_email_user_id).count() > 0
+    if exists:
+        return render_template('error.html', message="This link has already been used."), 403  
     
+   
     return render_template('index.html')
 
 
@@ -100,23 +110,33 @@ def authorize():
 
         user_email = user_info.get('email')
         user_name = user_info.get('name')
+        user_id_guid = str(uuid.uuid4())
 
-        add_user = User(name=user_name,email=user_email)
+        message = None
+
+        sender_id = session.get('UNIQUE_USER_ID')
+        if not sender_id:
+            sender_id = None
+        else:
+            user = db.session.get(User,sender_id)
+            message = user.message
+
+        add_user = User(name=user_name,email=user_email,user_id=user_id_guid,sender_id = sender_id)
         db.session.add(add_user)
         db.session.commit()
 
-        user_id = add_user.id
+        unique_user_id = add_user.user_id
 
-        session['user_email'] = user_info.get('email')
-        session['user_name'] = user_info.get('name')
+        session['user_email'] = user_email
+        session['user_name'] = user_name
         session['access_token'] = token.get('access_token')
 
 
-        final_email = session.get('user_name_message')
+        final_email = session.get('UNIQUE_USER_ID')
         if not final_email:
-            return render_template('sent_letter.html', email=session['user_email'], user_name=session['user_name'], user_id = user_id)
+            return render_template('sent_letter.html', email=session['user_email'], user_name=session['user_name'], user_id = unique_user_id)
         else:
-            return render_template('letter.html', email=session['user_email'], user_name=session['user_name'], user_id = user_id)
+            return render_template('letter.html', email=session['user_email'], user_name=session['user_name'], user_id = unique_user_id, message = message)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
@@ -128,30 +148,39 @@ def send_email_link_route():
     data = request.get_json()
     user_email_link = data.get("email")
 
-    user_id = data.get("user_id")
-    user = db.session.get(User, user_id)
+    unique_user_id = data.get("user_id")
+    message = data.get("message")
+    user = db.session.get(User, unique_user_id)
+    
+    user.message = message
+    user.email_to = user_email_link
+
     if not user:
         return jsonify({"message": "Error: User not found"}), 404
-    user.email_to = user_email_link
     db.session.commit() 
     
     if not access_token:
         return jsonify({"message": "Error: User not authenticated"}), 400
 
-    success = send_email_link(access_token, user_email_link)
+    success = send_email_link(access_token, user_email_link,unique_user_id)
     return jsonify({"message": "Email sent successfully!"}) if success else jsonify({"message": "Failed to send email"}), 500
 
-def send_email_link(access_token, user_email_link):
+def send_email_link(access_token, user_email_link,unique_user_id):
 
+    #this is sender user_email
     user_email = session.get('user_email')
 
     subject = "Open the link for suprise"
 
-    encrypted_email = encrypt_email(user_email)
+    unique_user_id = unique_user_id
+    encrypted_user_id = encrypt_email(unique_user_id)
     BACKEND_URL = "https://bemyvalentine-v1.onrender.com"
     # BACKEND_URL = "http://127.0.0.1:5000"
 
-    link = f"{BACKEND_URL}/{encrypted_email}"
+    # link = f"{BACKEND_URL}/{encrypted_email}" 
+    link = f"{BACKEND_URL}/{encrypted_user_id}" 
+    print("this is link = ",link)
+
     body = f"Hello,\nPlease go to this link: \n{link}"
 
     message = MIMEText(body)
@@ -173,23 +202,29 @@ def send_email_link(access_token, user_email_link):
 @app.route('/send_email', methods=['POST'])
 def send_email_route():
     access_token = session.get('access_token')
-    # user_email = session.get('user_email')    
-    final_email = session.get('user_name_message')
-
 
     data = request.get_json()
-    user_id = data.get("user_id")
-    response = data.get('response')
+    
+    respondent_user_id = data.get("user_id")
 
+    
+    user_data = db.session.get(User,respondent_user_id)
+    
+    response = data.get('response')
+    user_id = session.get('UNIQUE_USER_ID')
 
     user=db.session.get(User,user_id)
+    final_email = user.email
+    
+    #this is to add the from email in the receiver tab
+    user_data.email_from = final_email
+    
+    
     if not user:
         return jsonify({"message": "Error: User not found"}), 404
 
-    user.email_from = final_email
-    user.response = response
+    user_data.response = response
     db.session.commit()
-
 
     if not final_email or not access_token:
         return jsonify({"message": "Error: User not authenticated"}), 400
