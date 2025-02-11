1a4682d08f019b10cd1d39abc2710948d2e1c6b9
Raz-at <razbaidya1@gmail.com>
2025-02-11 16:27:46 +0545

    changes in d

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(200), primary_key=True)
+    sender_id = db.Column(db.String(200))
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=False, nullable=False)
     email_to = db.Column(db.String(100))
     email_from = db.Column(db.String(100))
     response = db.Column(db.String(100))
+    message = db.Column(db.String)  
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
